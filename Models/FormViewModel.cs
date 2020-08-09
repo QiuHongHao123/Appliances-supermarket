2d46d907602057ef400889bc2848a9437fb32c34
Honghao Qiu <3158874848@qq.com>
2020-08-09 18:23:28 +0800

    Register view and model

---
+++ Models/FormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FIT5032_assignment.Models
+{
+    public class FormViewModel
+    {
+    }
+    public class RegisterView {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+  
+        public string Email { get; set; }
+        public string Address { get; set; }
+    } 
+}
